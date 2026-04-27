e6672435f4d23f3e236046e57af5e0e050c28983
065Indraaa <indrbchtr06@gmail.com>
2026-04-27 23:03:46 +0700

    m

--- src/components/ScammerList.jsx
+++ src/components/ScammerList.jsx
@@ -5,6 +5,7 @@ const SCAMMER_DATA = {
     { username: '@fadeproXBT', platform: 'X', link: 'https://x.com/fadeproXBT' },
     { username: '@Hopium_papa', platform: 'X', link: 'https://x.com/Hopium_papa' },
     { username: '@fabolousgringo', platform: 'X', link: 'https://x.com/fabolousgringo' },
+    { username: '@zeusyxyz', platform: 'X', link: 'https://x.com/zeusyxyz' },
   ],
   tiktok: [
     { username: '@hussainyn2', platform: 'TikTok', link: 'https://www.tiktok.com/@hussainyn2' },
